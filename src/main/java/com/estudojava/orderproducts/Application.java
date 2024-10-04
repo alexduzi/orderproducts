3642720227e840d3e2d55f68dd7854fcee872480
Alex Duzi <alex.henrique.duzi>
2024-10-04 18:16:10 -0300

    criação do projeto

---
+++ src/main/java/com/estudojava/orderproducts/Application.java
@@ -0,0 +1,13 @@
+package com.estudojava.orderproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
