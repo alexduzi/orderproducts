937b9b55b1558906a38234a36ff7b7dbf4ff0702
Alex Duzi <alex.henrique.duzi>
2024-10-06 18:30:36 -0300

    update users

--- src/main/java/com/estudojava/orderproducts/resources/UserResource.java
+++ src/main/java/com/estudojava/orderproducts/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,4 +61,12 @@ public class UserResource {
 		
 		return ResponseEntity.noContent().build();
 	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+		User updatedUser = userService.update(id, user);
+		
+		return ResponseEntity.ok().body(updatedUser);
+	}
+	
 }
