fa8fe2a74335c7cc02626e93f7fd2e19a14a5ed0
Alex Duzi <alex.henrique.duzi>
2024-10-06 18:22:06 -0300

    delete user

--- src/main/java/com/estudojava/orderproducts/resources/UserResource.java
+++ src/main/java/com/estudojava/orderproducts/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,5 +52,12 @@ public class UserResource {
 					.toUri();
 		
 		return ResponseEntity.created(uri).body(insertedUser);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		userService.delete(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
